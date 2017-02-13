8e7d4a6b1f7c5733b8e24f76717df4010680b6d1
John Daly <jpdaly@gmail.com>
2017-02-13 16:52:03 +0000

    added count, add, and check if full to classroom class

--- Classroom.java
+++ Classroom.java
@@ -1,3 +1,36 @@
-class Classroom {
-  
+import java.util.ArrayList;
+
+public class Classroom {
+  private int roomNumber;
+  private ArrayList<Person> students;
+  private int studentLimit;
+
+
+  public Classroom(int roomNumber){
+    this.roomNumber = roomNumber;
+    this.students = new ArrayList<Person>();
+    this.studentLimit = 4;
+  }
+
+
+  public int getRoomNumber() {
+    return this.roomNumber;
+  }
+
+  public int studentCount() {
+    return students.size();
+  }
+
+  public Boolean classFull(){
+    return this.studentCount() == this.studentLimit;
+  }
+
+  public void addStudent(Person student){
+    students.add(student);
+  }
+
+  public void teacherWobbly(){
+    students.clear();
+  }
+
 }
--- ClassroomTest.java
+++ ClassroomTest.java
@@ -1,3 +1,62 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
+
+public class ClassroomTest {
+  Classroom room1;
+  Person john;
+  Person paul;
+  Person george;
+  Person ringo;
+  Person keith;
+
+
+  @Before
+  public void before() {
+    room1 = new Classroom(1);
+    john = new Person("John");
+    paul = new Person("Paul");
+    george = new Person("George");
+    ringo = new Person("Ringo");
+    keith = new Person("Keith");
+  }
+
+  @Test
+  public void hasRoomNumber() {
+    assertEquals(1, room1.getRoomNumber());
+  }
+
+  @Test
+  public void canCountStudents() {
+    assertEquals(0, room1.studentCount());
+  }
+
+  @Test
+  public void canAddStudents() {
+    room1.addStudent(john);
+    assertEquals(1, room1.studentCount());
+  }
+
+  @Test
+  public void detectsClassFull(){
+      room1.addStudent(john);
+      room1.addStudent(paul);
+      room1.addStudent(george);
+
+      assertEquals(false, room1.classFull());
+
+      room1.addStudent(ringo);
+      assertEquals(true, room1.classFull());
+  }
+
+  @Test 
+  public void teacherCanThrowWobblyAndEjectAllStudents() {
+    room1.addStudent(john);
+    room1.addStudent(paul);
+    room1.teacherWobbly();
+    assertEquals(0, room1.studentCount());
+  }
+
+  // @Test
+
+}
